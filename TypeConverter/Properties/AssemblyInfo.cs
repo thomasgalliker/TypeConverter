8df17b353564580ede16a6b54b7b21a6bafbccf6
thomasgalliker <thomas@bluewin.ch>
2015-09-25 00:00:02 +0200

    Added snk to TypeConverter.Tests project

--- TypeConverter/Properties/AssemblyInfo.cs
+++ TypeConverter/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("TypeConverter.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f1aadb1821f090" +
+[assembly: InternalsVisibleTo("TypeConverter.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f1aadb1821f090"+
                                                              "cd1d3ece956dd3e884762f9c9f2fdeb1684ec9b4689d6d4a66fb715ee2a40f2f3479b6aa4e60d2"+
                                                              "69b4539aebdec9412ce583d7c09661ec2084a5da465ee3a5d3e1901ada0ac9e877f7403011efd2"+
                                                              "9be468378b136873cc8a700f68d0672eee60a4626f5c80618c61d9a63f9cfcbcadb161653294ad"+
