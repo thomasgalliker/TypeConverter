66c1b80f7cb01f93f28d0cc4b7c5141b281ac8a3
thomasgalliker <thomas@bluewin.ch>
2016-09-17 22:28:15 +0200

    Add StringToGuidConverter

---
+++ TypeConverter.Tests/Converters/StringToGuidConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using FluentAssertions;
+
+using TypeConverter.Converters;
+
+using Xunit;
+
+namespace TypeConverter.Tests.Converters
+{
+    public class StringToGuidConverterTests
+    {
+        [Fact]
+        public void ShouldConvertFormatBStringToGuid()
+        {
+            // Arrange
+            const string InputString = "{1E20D9BB-D64C-4449-AC1B-36CB690601ED}";
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<string, Guid>(() => new StringToGuidConverter());
+
+            // Act
+            var outputGuid = converterRegistry.Convert<string, Guid>(InputString);
+
+            // Assert
+            outputGuid.Should().Be(new Guid(InputString));
+        }
+
+        [Fact]
+        public void ShouldConvertFormatNStringToGuid()
+        {
+            // Arrange
+            const string InputString = "4568CA6400E742BAAA41E76916DE7118";
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<string, Guid>(() => new StringToGuidConverter());
+
+            // Act
+            var outputGuid = converterRegistry.Convert<string, Guid>(InputString);
+
+            // Assert
+            outputGuid.Should().Be(new Guid(InputString));
+        }
+
+        [Fact]
+        public void ShouldConvertGuidToBFormatString()
+        {
+            // Arrange
+            var inputGuid = new Guid("83EDDA8A-4538-4BA8-8D40-E82C561CD745");
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<Guid,string>(() => new StringToGuidConverter());
+
+            // Act
+            var outputString = converterRegistry.Convert<Guid, string>(inputGuid);
+
+            // Assert
+            outputString.Should().Be("{83edda8a-4538-4ba8-8d40-e82c561cd745}");
+        }
+    }
+}
---
+++ TypeConverter/Converters/StringToGuidConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeConverter.Converters
+{
+    public class StringToGuidConverter : ToStringFormattableConvertable<Guid>, IConvertable<string, Guid>
+    {
+        protected override string Format { get { return "B"; } } // https://msdn.microsoft.com/de-de/library/s6tk2z69(v=vs.110).aspx
+
+        public Guid Convert(string value)
+        {
+            return new Guid(value);
+        }
+    }
+}
