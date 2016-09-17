546f11f68eaf2918017014b1a4f0926dad2bf549
thomasgalliker <thomas@bluewin.ch>
2016-09-17 21:53:39 +0200

    Add StringToFloatConverter

---
+++ TypeConverter.Tests/Converters/StringToFloatConverterTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+
+using TypeConverter.Converters;
+
+using Xunit;
+
+namespace TypeConverter.Tests.Converters
+{
+    public class StringToFloatConverterTests
+    {
+        [Fact]
+        public void ShouldConvertFloatMaxValueToString()
+        {
+            // Arrange
+            float inputFloat = float.MaxValue;
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<float, string>(() => new StringToFloatConverter());
+
+            // Act
+            var outputString = converterRegistry.Convert<float, string>(inputFloat);
+
+            // Assert
+            outputString.Should().Be("3.40282347E+38");
+        }
+
+        [Fact]
+        public void ShouldConvertFloatMinValueToString()
+        {
+            // Arrange
+            float inputFloat = float.MinValue;
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<float, string>(() => new StringToFloatConverter());
+
+            // Act
+            var outputString = converterRegistry.Convert<float, string>(inputFloat);
+
+            // Assert
+            outputString.Should().Be("-3.40282347E+38");
+        }
+
+        [Fact]
+        public void ShouldConvertStringToFloatMaxValue()
+        {
+            // Arrange
+            const string InputString = "3.40282347E+38";
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<string, float>(() => new StringToFloatConverter());
+
+            // Act
+            var outputFloat = converterRegistry.Convert<string, float>(InputString);
+
+            // Assert
+            outputFloat.Should().Be(float.MaxValue);
+        }
+
+        [Fact]
+        public void ShouldConvertStringToFloatMinValue()
+        {
+            // Arrange
+            const string InputString = "-3.40282347E+38";
+            IConverterRegistry converterRegistry = new ConverterRegistry();
+            converterRegistry.RegisterConverter<string, float>(() => new StringToFloatConverter());
+
+            // Act
+            var outputFloat = converterRegistry.Convert<string, float>(InputString);
+
+            // Assert
+            outputFloat.Should().Be(float.MinValue);
+        }
+    }
+}
---
+++ TypeConverter/Converters/StringToFloatConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace TypeConverter.Converters
+{
+    public class StringToFloatConverter : ToStringFormattableConvertable<float>, IConvertable<string, float>
+    {
+        protected override string Format { get { return "R"; } } // https://msdn.microsoft.com/en-us/library/dwhawy9k(v=vs.110).aspx#RFormatString
+
+        public float Convert(string value)
+        {
+            return float.Parse(value, CultureInfo.InvariantCulture);
+        }
+    }
+}
